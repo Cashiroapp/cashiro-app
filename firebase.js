cc9fbaf4acd75ca605b48edc954114256853861b
samaya1016025 <santiago.amaya1016@gmail.com>
2026-06-22 15:08:01 -0500

    Mostrar errores reales de Google Auth

--- firebase.js
+++ firebase.js
@@ -9,20 +9,35 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const auth     = firebase.auth();
-user = await Promise.race([
-  getCurrentUser(),
-  new Promise(resolve => setTimeout(() => resolve(null), 5000))
-]);
+auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+  .catch(err => console.error('Persistence error:', err));
 const db       = firebase.firestore();
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
+
+//async function loginGoogle() {
+  //try {
+    //const result = await auth.signInWithPopup(provider);
+   // return result.user;
+  //} catch(e) {
+ //   console.error('loginGoogle error:', e.code, e.message);
+ //   return null;
+ // }
+//}
 
 async function loginGoogle() {
   try {
     const result = await auth.signInWithPopup(provider);
     return result.user;
   } catch(e) {
+
     console.error('loginGoogle error:', e.code, e.message);
+
+    alert(
+      'Código:\n' + e.code +
+      '\n\nMensaje:\n' + e.message
+    );
+
     return null;
   }
 }
